dbcc77e762ea4d85e7fc77f4892f3a009c39cdc8
anderskambenders <a.zetsmanis@mail.ru>
2023-05-22 00:17:44 +0300

    feat: add selfrating

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -277,3 +277,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
   gameField.addEventListener('click', cellHandler);
 });
+
+function selfRating() {
+  // eslint-disable-next-line no-console
+  console.log(`
+  Score: 140/ 180
+  Выполненные пункты
+
+- [x]  layout, design, responsive UI: +10
+- [x]  at the beginning state of the game, the frame has size 10x10 and is filled with unopened cells. Should be 10 mines on field by default: +10
+- [x]   when user click on cells - it should be opened and marked as one of the following state: empty cell, cell with number, or cell with mine: +10
+- [x]  the game should end when the player reveals all cells that do not contain mines (win) or clicks on mine (lose) and related message is displayed at the end of the game: +10
+- [x]   mines are placed after the first move, so that user cannot lose on the first move. +20
+- [x]   user can mark “mined” cells using flags so as not to accidentally open them displaying the number of mines remaining and displaying number of used flags: +10
+- [x]   the game should use color coding (using numbers and colors) to indicate the number of mines surrounding a revealed cell: +10
+- [x]   the game can be restarted without reloading the page: +10
+- [x]   game duration and number of clicks are displayed: +15
+- [x]   when user opens a square that does not touch any mines, it will be empty and the adjacent squares will automatically open in all directions until reaching squares that contain numbers: +15
+- [x]   sound accompaniment (on/off) when clicking on cell and at the end of the game: +10
+- [x]  implemented saving the latest 10 results using LocalStorage: +10
+
+  Невыполненные пункты
+
+- [x]  implement ability to change the size (easy - 10x10, medium - 15x15, hard - 25x25) and number of mines for each size of the field (from 10 to 99): +20
+- [x]  implemented saving the state of the game: +10
+- [x]  option to choose different themes for the game board (dark/light themes): +10
+  `);
+}
+selfRating();
