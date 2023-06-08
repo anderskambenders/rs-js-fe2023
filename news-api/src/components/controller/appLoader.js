6281775accba8002dfea8262842db7c30cb404c0
anderskambenders <a.zetsmanis@mail.ru>
2023-06-08 20:47:29 +0300

    feat: add api key

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'a261ff31e58845e099b54dcca4ab6fcc', // получите свой ключ https://newsapi.org/
         });
     }
 }
