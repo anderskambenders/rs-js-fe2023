b16ba89a9d8942683dda4406a2ae8408ffcc52c1
anderskambenders <a.zetsmanis@mail.ru>
2023-06-09 15:44:53 +0300

    feat: migrate news.js to ts

---
+++ news-api/src/components/interfaces/index.ts
@@ -0,0 +1,10 @@
+export interface INews {
+  author: string;
+  content: string;
+  publishedAt: string;
+  description: string;
+  source: { id: string; name: string };
+  title: string;
+  url: string;
+  urlToImage: string;
+}
