e537f0397ddb1affa67e8bdfc20f32e5a5f9380f
anderskambenders <a.zetsmanis@mail.ru>
2023-06-10 21:35:18 +0300

    feat: migrate appView to ts

---
+++ news-api/src/components/view/appView.ts
@@ -0,0 +1,25 @@
+import News from "./news/news";
+import Sources from "./sources/sources";
+import { INews, ISource } from "../interfaces";
+
+export class AppView {
+  private news: News;
+  private sources: Sources;
+
+  constructor() {
+    this.news = new News();
+    this.sources = new Sources();
+  }
+
+  drawNews(data: { articles: INews[] }): void {
+    const values = data?.articles ? data?.articles : [];
+    this.news.draw(values);
+  }
+
+  drawSources(data: { sources: ISource[] }): void {
+    const values = data?.sources ? data?.sources : [];
+    this.sources.draw(values);
+  }
+}
+
+export default AppView;
