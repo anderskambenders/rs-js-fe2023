18188ccb5d8fd13984898b4a0833b4492c0da3aa
anderskambenders <a.zetsmanis@mail.ru>
2023-06-28 23:55:53 +0300

    feat: add animation for selected elems

--- rs-css/src/components/view/tableView.ts
+++ rs-css/src/components/view/tableView.ts
@@ -4,13 +4,17 @@ export class TableView {
   draw(currentLevel: number) {
     const table = document.querySelector('.table__main');
     const images = levels[currentLevel].img;
+    const select = levels[currentLevel].selector;
 
-    images.forEach((image) => {
+    images.forEach((image, ind) => {
       const imgWrapper = document.createElement('div');
       const img = document.createElement('img');
       img.src = image;
       imgWrapper.classList.add('image__wrapper');
       img.classList.add('image');
+      if (select.includes(ind)) {
+        img.classList.add('active');
+      }
       imgWrapper.append(img);
       table?.append(imgWrapper);
     });
